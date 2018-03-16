52904670c896835317f068b50bc925ce17f7365e
Solemon <solemon.faiz1@gmail.com>
2018-03-16 13:37:45 -0400

    another chnage

--- hello.py
+++ hello.py
@@ -1,3 +1,3 @@
 #!/Users/solemon/anaconda/bin/python
-print "hello world this is my first github project"
+print 'hello world this is my first github project'
 print " somcxcxcxcxe more stuuf added"
